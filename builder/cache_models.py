c4c51b18ba26ad540ba4c95356cc003a3614c2ae
alexpashkin <unsanny@gmail.com>
2023-07-17 23:09:06 +0300

    Try to change token auth logic

--- builder/cache_models.py
+++ builder/cache_models.py
@@ -1,18 +1,22 @@
 # builder/model_fetcher.py
 
+import os
 import torch
 from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
 
 def get_diffusion_pipelines():
+    print(F'TOKEN = {os.environ["HUGGING_FACE_HUB_TOKEN"]}')
     pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-0.9", 
                                                      torch_dtype=torch.float16, 
                                                      variant="fp16", 
+                                                     use_auth_token=os.environ["HUGGING_FACE_HUB_TOKEN"],
                                                      use_safetensors=True)
 
 
     refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-0.9", 
                                                                torch_dtype=torch.float16, 
                                                                use_safetensors=True, 
+                                                               use_auth_token=os.environ["HUGGING_FACE_HUB_TOKEN"],
                                                                variant="fp16")
     
     return pipe, refiner
