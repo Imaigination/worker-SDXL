faeecffda1f296c0d1f5afdc88ce7cf2a1773e87
alexpashkin <unsanny@gmail.com>
2023-07-18 11:51:34 +0300

    Add auth method

--- builder/cache_models.py
+++ builder/cache_models.py
@@ -2,10 +2,13 @@
 
 import os
 import torch
+import huggingface_hub
 from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
 
 def get_diffusion_pipelines():
     print(F'TOKEN = {os.environ["HUGGING_FACE_HUB_TOKEN"]}')
+    from huggingface_hub.hf_api import HfFolder;
+    HfFolder.save_token(os.environ["HUGGING_FACE_HUB_TOKEN"])
     pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-0.9", 
                                                      torch_dtype=torch.float16, 
                                                      variant="fp16", 
